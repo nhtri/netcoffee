64608323bee92a93e1347cbb94fddb809898a260
Tri Nguyen <tri.nh@hotmail.com>
2019-12-12 02:07:44 +0700

    z

--- server.js.js
+++ server.js.js
@@ -6,11 +6,11 @@ const app = express();
 
 // Serve only the static files form the dist directory
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/netcoffeeclient'));
+app.use(express.static(__dirname + '/netcoffeeclient'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/netcoffeeclient/index.html'));
+res.sendFile(path.join(__dirname+'/netcoffeeclient/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
