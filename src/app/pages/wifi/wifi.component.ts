4fb4192698c87d7616c2573be3cf923de2870a5e
Tri Nguyen <tri.nh@hotmail.com>
2019-12-29 07:40:03 +0700

    new wifi

--- src/app/pages/wifi/wifi.component.ts
+++ src/app/pages/wifi/wifi.component.ts
@@ -3,6 +3,8 @@ import Chart from 'chart.js';
 import { NetworkserviceService } from 'src/app/services/networkservice.service';
 import { network } from 'src/app/components/model/network';
 import { from } from 'rxjs';
+import { FormGroup, FormBuilder, FormControl } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-wifi',
@@ -11,15 +13,21 @@ import { from } from 'rxjs';
 })
 export class WifiComponent implements OnInit {
   data: network[] = [];
-
+  wifiform: FormGroup | any;
   cols: any[];
-
+wifidata:any
+luuwifidata:any
   clonedData: {} = {};
   displayDialog: boolean;
 
   constructor(
+    private formBuilder: FormBuilder,
     private networkserviceService: NetworkserviceService,
-  ) { }
+    private router: Router
+  ) { 
+    this.initForm();
+    this.onFormChanges();
+  }
 
   ngOnInit() {
     this.cols = [
@@ -35,6 +43,24 @@ export class WifiComponent implements OnInit {
 console.log(this.data)
 
   }
+
+  initForm() {
+    this.wifiform = this.formBuilder.group({
+      mawifi: new FormControl(null),
+      sdtsim: new FormControl(null),
+      masim: new FormControl(null),
+     
+    })}
+
+    onFormChanges() {
+      this.wifiform.valueChanges.subscribe(res => {
+ 
+       this.wifidata=res
+        console.log("wifiform:", res)
+      });
+  
+     }
+
   selectNetWithButton(value) {
     console.log(value)
   }
@@ -58,4 +84,30 @@ showDialogToAdd() {
 
   this.displayDialog = true;
 }
+
+save(){
+  if (this.wifidata.mawifi && this.wifidata.masim && this.wifidata.sdtsim ) {
+    this.luuwifidata = [
+      this.wifidata.mawifi,
+      this.wifidata.sdtsim,
+      this.wifidata.masim
+    ]
+    this.networkserviceService.postAllUser(this.luuwifidata).subscribe(
+      data => {
+        alert("Lưu Thành Công");
+        this.router.navigateByUrl('wifi')
+        console.log("POST Request is successful ", data);
+      },
+      error => {
+
+        console.log("Error", error);
+
+      })
+  }
+  else{
+    alert("Điền thông tin vào ô * trống");
+  }
+}
+
+
 }
